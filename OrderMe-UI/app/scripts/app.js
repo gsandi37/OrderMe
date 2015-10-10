274eb5d59f75e8186dd8947ac59e81e4ca2fb7fd
gsandi37 <itsallangular.37@gmail.com>
2015-10-10 17:20:13 -0500

    Skeleton structure of OrderMe-UI.

---
+++ OrderMe-UI/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name orderMeUiApp
+ * @description
+ * # orderMeUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('orderMeUiApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ OrderMe-UI/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orderMeUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the orderMeUiApp
+ */
+angular.module('orderMeUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
